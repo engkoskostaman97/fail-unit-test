b1cfa62a78c4459942b64c560a7439ac7ff7cabe
engkos ragda <engkos.ragda@gmail.com>
2023-04-21 16:44:55 +0700

    fail unit test

--- hello_world_test.go
+++ hello_world_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Eko")
-	
+
 	if result != "Hello Eko" {
 		//unit test failed
-		panic("Result is not Hello Eko")
+		t.Error("Data ini tidak failed")
 	}
 }
